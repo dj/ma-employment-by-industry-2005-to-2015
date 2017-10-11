4d93a27a403a78fd4e6f36f5b420a490247d0cd9
DJ Hartman <danieljayhartman@gmail.com>
2017-10-11 12:53:50 -0400

    Update transformData

--- src/index.js
+++ src/index.js
@@ -4,22 +4,28 @@ import { SmallMultiplesVis } from './small-multiples-vis'
 import './index.scss'
 
 function transformData(rows) {
-	const data = rows
-		.filter(r => {
-			return r.Industry.includes('employment')
-		})
-		.map(row => {
-			const industry = row.Industry,
-				values = Object.entries(row)
-					.slice(1)
-					.map(d => [new Date(d[0]), d[1]])
-			return {
-				industry,
-				values
-			}
+	const data = rows.map(row => {
+		const industry = row.Industry
+		const min = row.Min
+		const max = row.Max
+
+		delete row.Industry
+		delete row.Min
+		delete row.Max
+
+		// The remaining keys in row are all dates, convert to an array of [Date, Number]
+		const values = Object.entries(row).map(d => {
+			return [new Date(d[0]), d[1]]
 		})
 
-	console.log(data)
+		return {
+			industry,
+			min,
+			max,
+			values
+		}
+	})
+
 	return data
 }
 
